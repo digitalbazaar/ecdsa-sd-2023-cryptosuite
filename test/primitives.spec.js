ad69b325eceda76b4e77c941b0abeee41b33f60b
Dave Longley <dlongley@digitalbazaar.com>
2023-05-17 16:28:26 -0400

    Refactor `split` into `group` operation.

--- test/primitives.spec.js
+++ test/primitives.spec.js
@@ -8,11 +8,11 @@ describe('di-sd-primitives', () => {
     it('it should have proper exports', async () => {
       should.exist(primitives);
       primitives.deskolemize.should.be.a('function');
-      primitives.filterAndSplit.should.be.a('function');
+      primitives.filterAndGroup.should.be.a('function');
+      primitives.group.should.be.a('function');
       primitives.hashMandatory.should.be.a('function');
       primitives.hmacIdCanonize.should.be.a('function');
       primitives.skolemize.should.be.a('function');
-      primitives.split.should.be.a('function');
       primitives.toDeskolemizedRDF.should.be.a('function');
     });
   });
